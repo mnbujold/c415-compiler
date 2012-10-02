aa95b83ceaf565677529a6dcbc87f5da0d667eaf
James Osgood <jamesosogood@gmail.com>
2012-10-02 17:32:28 -0600

    Moved, updated, and added some comments to runner.py.

--- tests/runner.py
+++ tests/runner.py
@@ -1,18 +1,23 @@
 """
+Author: James Osgood
+
 Call with a list of integers representing .pal files to test those files
-using the lex program. Appends results to the tests.log file.
+using the pal compiler program. Appends results to the tests.log file.
 
 Example usage:
 $> python runner.py 0 1 2
-# Runs the 0.pal, 1.pal, and 2.pal files located in the tests/ directory.
+# Runs the 0.pal, 1.pal, and 2.pal files.
 $> python runner.py --all
-# Runs all the .pal test files in the tests/ directory (not implemented yet).
+# Runs all the .pal test files (not implemented yet).
 
 """
 
 from argparse import ArgumentParser
 from subprocess import call
 from datetime import datetime
+
+LOG_NAME = 'tests.log'
+PAL_PATH = '../lex'
 
 def _all_tests(test_list):
     print 'not implemented yet ...'
@@ -23,10 +28,10 @@ def _run_tests(test_list):
                  'TIME: %s\n'\
                  'TEST: %d.pal\n'\
                  '----------------------\n'
-    run_cmds = ['./lex']
-    test_path = 'tests/%d.pal'
+    run_cmds = [PAL_PATH]
+    test_path = '%d.pal'
     
-    output_log = open('tests.log', 'a+')
+    output_log = open(LOG_NAME, 'a+')
     
     for test_index in test_list:
         test_file = open(test_path % test_index, 'r')
@@ -34,19 +39,19 @@ def _run_tests(test_list):
         start_date = start_datetime.date().isoformat()
         start_time = start_datetime.time().isoformat()
         
-        output_log = open('tests.log', 'a+')
+        output_log = open(LOG_NAME, 'a+')
         output_log.write(output_msg % (start_date, start_time, test_index))
         output_log.close() # so the file is written to in the correct order
         
-        output_log = open('tests.log', 'a+')
+        output_log = open(LOG_NAME, 'a+')
         call(run_cmds, stdin = test_file, stdout = output_log)
         test_file.close()
         output_log.close()
 
 def _get_cmdline_args():
-    dscrp_txt = 'run *.pal test programs in the tests/ directory'
+    dscrp_txt = 'run *.pal test programs'
     help_name_txt = 'a name of an [0-9].pal test'
-    help_all_txt = 'run all *.pal tests (default: false)'
+    help_all_txt = 'run all *.pal tests (default: false) (NOT YET IMPLEMENTED)'
     
     parser = ArgumentParser(description = dscrp_txt)
     parser.add_argument('test_names',
