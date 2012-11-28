a8e495cc610c5a2fc1df0f9b0430465c3ac5be30
dchui1 <dchui1@ualberta.ca>
2012-11-28 13:00:35 -0700

    Added builtinasc header so we can call them

---
+++ src/builtinasc.h
@@ -0,0 +1,14 @@
+/**
+ * header file to interface with the ASC builtin code
+ * Author: Daniel Chui, Mike Bujold
+ * 
+ */
+#ifndef BUILTINASCGEN_H
+#define BUILTINASCGEN_H
+
+
+#define ABSI_REGISTER 0
+#define ABSR_REGISTER 0
+
+
+#endif
